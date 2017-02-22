85de43e0e363ae4ba924ad4ed69efd279f089b4a
Gregory Brine <greg.brine@scrmhub.com>
2017-02-22 22:55:44 +1100

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,9 @@
 <?php
+//Your AWS key
 define('AWS_KEY',           'YOUR_KEY');
+
+//Your AWS Secret
 define('AWS_SECRET',        'YOUR SECRET');
+
+//The name of the bucket to upload to
 define('AWS_BUCKET_NAME',   'YOUR BUCKET NAME');
